2b2ee18cd786fb345203279de4ddcd8aed2c54f7
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-07-16 00:28:32 -0300

    Basic functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+BoringFramework is a collection of assorted things I use regularly in a lot of projects. 
+
+This library allows to use the API notifications (https://developer.mozilla.org/en-US/docs/Web/API/notification) in Blazor apps.
+
+Nuget: BoringFramework-Notifications
---
+++ src/Notifications/wwwroot/notifications.js
@@ -0,0 +1,17 @@
+﻿window.blazorNotifications = {
+    browserSupportsNotifications: function () {
+        return "Notification" in window;
+    },
+
+    permissionStatus: function () {
+        return Notification.permission;
+    },
+
+    requestPermission: function () {
+        Notification.requestPermission();
+    },
+
+    sendNotification: function (message) {
+        new Notification(message);
+    }
+};
